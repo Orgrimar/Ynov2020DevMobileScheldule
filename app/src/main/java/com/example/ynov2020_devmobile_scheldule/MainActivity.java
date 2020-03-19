74d0bdef875772d1b43e04092e81324437a9eede
Orgrimar <polatoj@gmail.com>
2020-03-19 18:08:24 +0100

    Authentification par mail

--- app/src/main/java/com/example/ynov2020_devmobile_scheldule/MainActivity.java
+++ app/src/main/java/com/example/ynov2020_devmobile_scheldule/MainActivity.java
@@ -1,60 +1,97 @@
+//Documention authentification par mail
+//https://github.com/firebase/quickstart-android/blob/4b18921c3a4aa0fd3dcaaa053359c9452e430d1a/auth/app/src/main/java/com/google/firebase/quickstart/auth/java/EmailPasswordActivity.java#L132-L159
 package com.example.ynov2020_devmobile_scheldule;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.Tag;
 
-import java.util.concurrent.TimeUnit;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity<mCallbacks> extends AppCompatActivity {
+    private static final String TAG = "EmailPassword";
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    private EditText mVerificationField;
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
-        }
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResults) {
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.i("App", "Permission granted");
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void OnClick(View view) {
+        final EditText mail = (EditText) findViewById(R.id.mailText);
+        final EditText password = (EditText) findViewById(R.id.passwordText);
+        final CheckBox isParent = (CheckBox) findViewById(R.id.isParentCheckBox);
+
+        mAuth.signInWithEmailAndPassword(mail.getText().toString(), password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                final String TAG = "SUCCESS";
+                Log.d(TAG, "signInWithEmail : success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                final String TAG = "FAILED";
+                Log.w(TAG, "signInWithEmail : failure", task.getException());
+                Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        //hideProgressBar();
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            findViewById(R.id.emailPasswordButtons).setVisibility(View.GONE);
+            findViewById(R.id.emailPasswordFields).setVisibility(View.GONE);
+            findViewById(R.id.signedInButtons).setVisibility(View.VISIBLE);
+
+            if (user.isEmailVerified()) {
+                findViewById(R.id.verifyEmailButton).setVisibility(View.GONE);
+            } else {
+                findViewById(R.id.verifyEmailButton).setVisibility(View.VISIBLE);
+            }
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.emailPasswordButtons).setVisibility(View.VISIBLE);
+            findViewById(R.id.emailPasswordFields).setVisibility(View.VISIBLE);
+            findViewById(R.id.signedInButtons).setVisibility(View.GONE);
         }
     }
 }
